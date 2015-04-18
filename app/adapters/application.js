a1096806e55e711b6a6e9e4cb8e7abae28558f6c
Edgar Lee <edgarhinshunlee@gmail.com>
2015-04-17 20:30:48 -0400

    Fix host to default to API host from environment variables

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.ActiveModelAdapter.extend({
-  host: 'localhost',
+  host: config.apiHost,
   namespace: config.apiNamespace
 });
