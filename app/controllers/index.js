584f90ec7ec8e2e95b641b57ef934a0338cc3145
Edgar Lee <edgarhinshunlee@gmail.com>
2015-04-25 17:47:58 -0400

    Add async property to owner association, and autologin after creation of account

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,8 +3,14 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     register: function() {
+      var _this = this;
+
       this.get('model').save().then(function() {
-        alert('Successful create!');
+        var credentials = {
+          identification: _this.get('model.email'),
+          password: _this.get('model.password')
+        };
+        _this.get('session').authenticate('simple-auth-authenticator:jwt', credentials);
       }, function() {
         alert('Failed to create');
       });
--- app/models/account.js
+++ app/models/account.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  owner: DS.belongsTo('accountable', { polymorphic: true }),
+  owner: DS.belongsTo('accountable', { polymorphic: true, async: true }),
   competitions: DS.hasMany('competition', { async: true }),
 
   username: DS.attr('string')
